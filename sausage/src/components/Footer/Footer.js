ba2181ce225739b11a8175cf442f3877e87dc2b8
William Thorne <williamcthorne@gmail.com>
2021-06-11 13:11:45 -0700

    Footer, Footer Links, FooterWrapper, FooterContainer, FooterLinkItems

--- sausage/src/components/Footer/Footer.js
+++ sausage/src/components/Footer/Footer.js
@@ -1,5 +1,19 @@
 import React from 'react'
-import {FooterContainer, FooterSubscription, FooterSubHeading, FooterSubText} from './Footer.elements';
+import {
+    FooterContainer, 
+    FooterSubscription, 
+    FooterSubHeading, 
+    FooterSubText,
+    Form,
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLink,
+    FooterLinksItems,
+    FooterLinkTitle
+
+} from './Footer.elements';
+import { Button } from '../../globalStyles'
 
 const Footer = () => {
     return (
@@ -11,7 +25,42 @@ const Footer = () => {
                 <FooterSubText>
                     More Text
                 </FooterSubText>
+                <Form>
+                    <FormInput name='email' type='email' placeholder="Your Email"/>
+                        <Button fontBig>Subscribe</Button>
+                </Form>
             </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>
+                            About Us
+                        </FooterLinkTitle>
+                        <FooterLink to='/about'>Our Story</FooterLink>
+                        <FooterLink to='/contact'>Contact Us</FooterLink>
+                        <FooterLink to='/'>Where to Find Us</FooterLink>
+                        <FooterLink to='/'>Shop</FooterLink>
+                    </FooterLinksItems>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>
+                            Contact Us
+                        </FooterLinkTitle>
+                        <FooterLink to='/about'>Our Story</FooterLink>
+                        <FooterLink to='/contact'>Contact Us</FooterLink>
+                        <FooterLink to='/'>Where to Find Us</FooterLink>
+                        <FooterLink to='/'>Shop</FooterLink>
+                    </FooterLinksItems>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>
+                            Social Media
+                        </FooterLinkTitle>
+                        <FooterLink to='/about'>Our Story</FooterLink>
+                        <FooterLink to='/contact'>Contact Us</FooterLink>
+                        <FooterLink to='/'>Where to Find Us</FooterLink>
+                        <FooterLink to='/'>Shop</FooterLink>
+                    </FooterLinksItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
         </FooterContainer>
     )
 }
