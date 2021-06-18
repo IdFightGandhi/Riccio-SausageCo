cde5621e52b9f658fe400f7102a7f5edfbbaa960
William Thorne <williamcthorne@gmail.com>
2021-06-18 11:06:00 -0700

    Navbar 3

--- sausage/src/components/Navbar/Navbar.js
+++ sausage/src/components/Navbar/Navbar.js
@@ -1,42 +1,47 @@
-import React from 'react'
-import Logo from '../../images/logo.jpg'
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
 import {
-    Nav,
-    Bars,
+    Nav, 
+    NavbarContainer, 
+    NavLogo,
+    MobileIcon,
     NavMenu,
+    NavItem,
     NavLink,
     NavBtn,
-    NavBtnLink,
-    Img
-}
-from './Navbar.elements'
+    NavBtnLink
+
+} from './Navbar.elements'
 
 const Navbar = () => {
     return (
+        <>
         <Nav>
-            <NavLink to="/">
-                <Img src={Logo} alt="logo"/>
-                
-            </NavLink>
-            <Bars />
-            <NavMenu>
-                <NavLink to="/" activeStyle>
-                    Shop 
-                </NavLink>
-                <NavLink to="/" activeStyle>
-                    Products
-                </NavLink>
-                <NavLink to="/" activeStyle>
-                    About
-                </NavLink>
-                <NavLink to="/" activeStyle>
-                    Contact
-                </NavLink>
-            </NavMenu>
-            <NavBtn>
-                <NavBtnLink to="/shop">Shop Coming Soon</NavBtnLink>
-            </NavBtn>
+            <NavbarContainer>
+                <NavLogo to='/'>
+                    stuff
+                </NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLink to="/products">Products</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="/about">About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink to="/contact">Contact</NavLink>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/store">Shop</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
         </Nav>
+
+        </>       
     )
 }
 
