55e66ac5ebd1a4eba9d1d48ebebc2708658db41d
William Thorne <williamcthorne@gmail.com>
2021-07-28 11:10:20 -0700

    ProductCarousel image redo

--- sausage/src/data.js
+++ sausage/src/data.js
@@ -1,11 +1,7 @@
-import image1 from './images/bowl.jpg'
-import image2 from './images/grill.jpg'
-import image3 from './images/logo.jpg'
-import image4 from './images/onions.jpg'
-import image5 from './images/plated.jpg'
-// import image7 from './images/sausage-demo.jpeg'
-import image8 from './images/sausage.png'
+import image1 from './images/image2.jpeg'
+import image2 from './images/image4.jpeg'
+import image3 from './images/sausage.png'
 // import image9 from './images/test.png'
 
-export const data = [image1,image2,image3,image4,image5,image8,];   
+export const data = [image1,image2,image3];   
 
