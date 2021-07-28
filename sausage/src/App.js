775192b86e9215dbde628c12806d6d53aef51485
William Thorne <williamcthorne@gmail.com>
2021-07-28 10:54:52 -0700

    reload image assets to carousels

--- sausage/src/App.js
+++ sausage/src/App.js
@@ -5,6 +5,9 @@ import { Navbar, Footer } from './components';
 import Home from './pages/HomePage/Home';
 import MainSlider from './components/MainSlider/MainSlider'
 import ProductCarousel from './components/ProductCarousel/ProductCarousel'
+import image1 from "../src/images/onions.jpg"
+import image2 from "../src/images/image0.jpeg"
+import image3 from "../src/images/quality.png"
 
 
 
@@ -14,7 +17,8 @@ function App() {
     <Router>
       <GlobalStyle />
       <Navbar />
-      <MainSlider />
+      <MainSlider 
+      images={[image1,image2, image3]}/>
       <ProductCarousel />
       <Switch>
         <Route path="/" exact component={Home} />
